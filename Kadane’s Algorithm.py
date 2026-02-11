1fd756b28b2b1420bd7a25e665ea0471368d1d05
krishnasharma011004-debug <krishnasharma011004@gmail.com>
2026-02-12 00:40:27 +0530

    Update Kadane’s Algorithm.py

--- Kadane’s Algorithm.py
+++ Kadane’s Algorithm.py
@@ -1,10 +1,1 @@
-class Solution:
-    def maxSubArraySum(self, arr):
-        max_current = arr[0]
-        max_global = arr[0]
-        
-        for i in range(1, len(arr)):
-            max_current = max(arr[i], max_current + arr[i])
-            max_global = max(max_global, max_current)
-        
-        return max_global
+
