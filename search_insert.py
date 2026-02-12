78692d798876fd54523ce8eb18247ad0d4e79c06
krishnasharma011004-debug <krishnasharma011004@gmail.com>
2026-02-12 12:08:04 +0530

    Update search_insert.py

--- search_insert.py
+++ search_insert.py
@@ -1,9 +1,11 @@
 class Solution:
-    def searchInsert(self, nums, target):
-        left, right = 0, len(nums) - 1
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
         
         while left <= right:
             mid = (left + right) // 2
+            
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
