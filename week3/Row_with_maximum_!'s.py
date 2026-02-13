421bb74ee3e9d60d98b5fc8523981426e37f5573
krishnasharma011004-debug <krishnasharma011004@gmail.com>
2026-02-13 09:58:04 +0530

    Create Row_with_maximum_!'s.py

---
+++ week3/Row_with_maximum_!'s.py
@@ -0,0 +1,12 @@
+class Solution10:
+    def rowWithMax1s(self, arr, n, m):
+        max_row = -1
+        max_count = 0
+        
+        for i in range(n):
+            count = sum(arr[i])
+            if count > max_count:
+                max_count = count
+                max_row = i
+        
+        return max_row
