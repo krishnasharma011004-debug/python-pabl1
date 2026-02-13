6389d3b0acb3ef8f215bf7ff3d84becabfe18855
krishnasharma011004-debug <krishnasharma011004@gmail.com>
2026-02-13 09:35:12 +0530

    Update common_elements_in_3_sorted_array.py

--- week2/common_elements_in_3_sorted_array.py
+++ week2/common_elements_in_3_sorted_array.py
@@ -1,23 +1,23 @@
-class Solution7:
-    def commonElements(self, A, B, C):
+class Solution:
+    def commonElements(self, arr1, arr2, arr3):
         i = j = k = 0
-        n1, n2, n3 = len(A), len(B), len(C)
+        n1, n2, n3 = len(arr1), len(arr2), len(arr3)
         result = []
-        
+
         while i < n1 and j < n2 and k < n3:
-            
-            if A[i] == B[j] == C[k]:
-                if not result or result[-1] != A[i]:
-                    result.append(A[i])
+
+            if arr1[i] == arr2[j] == arr3[k]:
+                if not result or result[-1] != arr1[i]:
+                    result.append(arr1[i])
                 i += 1
                 j += 1
                 k += 1
-            
-            elif A[i] < B[j]:
+
+            elif arr1[i] < arr2[j]:
                 i += 1
-            elif B[j] < C[k]:
+            elif arr2[j] < arr3[k]:
                 j += 1
             else:
                 k += 1
-        
+
         return result if result else [-1]
