93af14178ab5e2c55b8ca5a312036b56e4b2bd63
krishnasharma011004-debug <krishnasharma011004@gmail.com>
2026-02-13 09:29:54 +0530

    Update merge_two_soeted_array.py

--- week2/merge_two_soeted_array.py
+++ week2/merge_two_soeted_array.py
@@ -1,25 +1,26 @@
-class Solution5:
-    def merge(self, a, b, n, m):
+class Solution:
+    def mergeArrays(self, a, b):
+        n, m = len(a), len(b)
         gap = (n + m + 1) // 2
-        
+
         while gap > 0:
             i = 0
             j = gap
-            
-            while j < (n + m):
-                
-                if j < n and a[i] > a[j]:
-                    a[i], a[j] = a[j], a[i]
-                
-                elif i < n and j >= n and a[i] > b[j - n]:
-                    a[i], b[j - n] = b[j - n], a[i]
-                
-                elif i >= n and b[i - n] > b[j - n]:
-                    b[i - n], b[j - n] = b[j - n], b[i - n]
-                
+
+            while j < n + m:
+                if i < n and j < n:
+                    if a[i] > a[j]:
+                        a[i], a[j] = a[j], a[i]
+                elif i < n and j >= n:
+                    if a[i] > b[j - n]:
+                        a[i], b[j - n] = b[j - n], a[i]
+                else:
+                    if b[i - n] > b[j - n]:
+                        b[i - n], b[j - n] = b[j - n], b[i - n]
+
                 i += 1
                 j += 1
-            
+
             if gap == 1:
                 gap = 0
             else:
