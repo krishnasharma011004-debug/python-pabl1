75fa308231d68925e7af0ddf1d55dcc160ac5b9f
krishnasharma011004-debug <krishnasharma011004@gmail.com>
2026-02-13 10:20:48 +0530

    Update Row_with_maximum_1's.py

--- week3/Row_with_maximum_1's.py
+++ week3/Row_with_maximum_1's.py
@@ -1,12 +1,15 @@
 class Solution:
-    def rowWithMax1s(self, arr, n, m):
+    def rowWithMax1s(self, mat):
+        n = len(mat)
+        m = len(mat[0])
+
         max_row = -1
         max_count = 0
-        
+
         for i in range(n):
-            count = sum(arr[i])
+            count = sum(mat[i])
             if count > max_count:
                 max_count = count
                 max_row = i
-        
+
         return max_row
