d754709def918756ac7d932261ffc5eecb3622e8
krishnasharma011004-debug <krishnasharma011004@gmail.com>
2026-02-13 10:15:34 +0530

    Update Spiral_traversal_of_matrix.py

--- week3/Spiral_traversal_of_matrix.py
+++ week3/Spiral_traversal_of_matrix.py
@@ -1,27 +1,28 @@
 class Solution:
-    def spirallyTraverse(self, matrix, r, c):
-        result = []
+    def spirallyTraverse(self, matrix):
+        r = len(matrix)
+        c = len(matrix[0])
+
         top, bottom = 0, r - 1
         left, right = 0, c - 1
-        
+        result = []
+
         while top <= bottom and left <= right:
-            
             for i in range(left, right + 1):
                 result.append(matrix[top][i])
             top += 1
-            
             for i in range(top, bottom + 1):
                 result.append(matrix[i][right])
             right -= 1
-            
+
             if top <= bottom:
                 for i in range(right, left - 1, -1):
                     result.append(matrix[bottom][i])
                 bottom -= 1
-            
+
             if left <= right:
                 for i in range(bottom, top - 1, -1):
                     result.append(matrix[i][left])
                 left += 1
-        
+
         return result
