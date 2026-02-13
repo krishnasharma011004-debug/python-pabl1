d6db4fa12cc2dba127b8193b76a718e460c6e45e
krishnasharma011004-debug <krishnasharma011004@gmail.com>
2026-02-13 10:08:29 +0530

    Update Find_median_of_array.py

--- week3/Find_median_of_array.py
+++ week3/Find_median_of_array.py
@@ -1,9 +1,9 @@
-class Solution6:
+class Solution:
     def find_median(self, arr):
         arr.sort()
         n = len(arr)
-        
+
         if n % 2 == 1:
             return arr[n // 2]
         else:
-            return (arr[n // 2] + arr[n // 2 - 1]) / 2
+            return (arr[n // 2] + arr[n // 2 - 1]) // 2
