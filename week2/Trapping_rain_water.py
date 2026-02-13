532686c3f364964250ef917ad1130a150b0e3351
krishnasharma011004-debug <krishnasharma011004@gmail.com>
2026-02-13 09:40:58 +0530

    Update Trapping_rain_water.py

--- week2/Trapping_rain_water.py
+++ week2/Trapping_rain_water.py
@@ -1,16 +1,17 @@
-class Solution11:
-    def trappingWater(self, arr, n):
+class Solution:
+    def maxWater(self, arr):
+        n = len(arr)
         if n == 0:
             return 0
-        
+
         left = 0
         right = n - 1
         left_max = 0
         right_max = 0
         water = 0
-        
+
         while left <= right:
-            
+
             if arr[left] < arr[right]:
                 if arr[left] >= left_max:
                     left_max = arr[left]
@@ -23,5 +24,5 @@ class Solution11:
                 else:
                     water += right_max - arr[right]
                 right -= 1
-        
+
         return water
