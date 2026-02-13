57557b3ff725eb6db7ab18e46e31c83fc724d82b
krishnasharma011004-debug <krishnasharma011004@gmail.com>
2026-02-13 10:05:23 +0530

    Update Minimum_Swaps_Required_to_Bring_k_together.py

--- week3/Minimum_Swaps_Required_to_Bring <= k Together.py
+++ week3/Minimum_Swaps_Required_to_Bring <= k Together.py
@@ -1,24 +1,25 @@
 class Solution:
-    def minSwap(self, arr, n, k):
-        
+    def minSwap(self, arr, k):
+        n = len(arr)
+
         count = 0
         for num in arr:
             if num <= k:
                 count += 1
-        
+
         bad = 0
         for i in range(count):
             if arr[i] > k:
                 bad += 1
-        
+
         ans = bad
-        
+
         for i in range(0, n - count):
             if arr[i] > k:
                 bad -= 1
             if arr[i + count] > k:
                 bad += 1
-            
+
             ans = min(ans, bad)
-        
+
         return ans
